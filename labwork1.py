598186d2bc445573687fcbb121fb2d20eadc8477
dinhanhx <dinhanhx@proton.me>
2022-12-17 17:41:11 +0700

    Hmm

--- labwork1.py
+++ labwork1.py
@@ -1,6 +1,16 @@
-import numba
 from numba import cuda
+from numba.cuda.cudadrv import enums
+show = print
 
-cuda.detect()
-
-cuda.select_device(0)
+show(cuda.detect())
+show(cuda.devices)
+show(cuda.gpus)
+device = cuda.select_device(0)
+show(cuda.current_context().get_memory_info())
+my_sms = device.MULTIPROCESSOR_COUNT
+my_cc = device.compute_capability
+cores_per_sm = 64
+total_cores = cores_per_sm*my_sms
+show(f"GPU compute capability: {my_cc}")
+show(f"GPU total number of SMs: {my_sms}")
+show(f"total cores: {total_cores}")
